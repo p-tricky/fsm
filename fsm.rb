8fee633066d5e1bee4f37efbcbff0bb74df8d907
Patrick Loftus <patrickloftus@roam147-059.ceas.wmich.edu>
2015-02-08 21:49:40 -0500

    finished book problems

--- fsm.rb
+++ fsm.rb
@@ -1,8 +1,20 @@
+## 
+#  The fsm class builds a finite state machine from
+#  a text file, and 
+#
+#  The text files should be formatted as follows:
+#     starting state
+#     accepting states (comma separated)
+#     transitions (start, path, dest)
+#
 class FSM
 
   attr_reader :state, :accepting_states, :transitions
 
-  def initialize(state)
+  ## Documentation for a function
+  #
+  #  More details
+  def initialize(state=nil)
     @state = state
     @accepting_states = []
     @transitions = {}
@@ -24,26 +36,24 @@ class FSM
     @state = @transitions[@state][input]
   end
 
-  def self.get_array_of_words(line)
+  def self.parse_input_line_for_words(line)
     line.split(/\W+/).reject(&:empty?)
   end
 
-  def self.build_from_file(filename)
+  def build_from_file(filename)
     open(filename, 'r') do |f|
-      state = FSM.get_array_of_words(f.gets)[0]
-      fsm = FSM.new(state)
-      fsm.accept(FSM.get_array_of_words(f.gets))
+      @state = FSM.parse_input_line_for_words(f.gets)[0]
+      accept(FSM.parse_input_line_for_words(f.gets))
       while (line = f.gets) 
-        start, path, dest = FSM.get_array_of_words(line)
-        fsm.add_transition(start, path, dest)
+        start, path, dest = FSM.parse_input_line_for_words(line)
+        add_transition(start, path, dest)
       end
-      return fsm
     end
   end
       
   def self.get_transition(line, has_words)
     if has_words
-      transitions = self.get_array_of_words(line)
+      transitions = self.parse_input_line_for_words(line)
       transitions.size.times do |n|
         yield transitions[n]
       end
@@ -58,18 +68,14 @@ class FSM
     has_words = input.include?(",")
     begin
       FSM.get_transition(input, has_words) do |transition|
-        #puts "transition #{transition}"
         change_state(transition)
       end
       if @accepting_states.include?(@state)
-        #puts "final state #{@state}"
         return "Accept"
       else
-        #puts "final state #{@state}"
         return "Reject: invalid final state"
       end
     rescue NoMethodError
-      #puts "final state #{@state}"
       return "Reject: invalid transition"
     end
   end
@@ -100,7 +106,10 @@ class FSM
     fsm_file = ARGV[0]
     input_file = ARGV[1]
     output_file = ARGV[2]
-    fsm = FSM.build_from_file(fsm_file)
+    fsm = FSM.new
+    fsm.build_from_file(fsm_file)
     fsm.run_from_file(input_file, output_file)
   end
+
+  private :accept, :add_transition, :change_state
 end
--- test/tc_fsm.rb
+++ test/tc_fsm.rb
@@ -7,13 +7,13 @@ class TestFSM < Test::Unit::TestCase
     @fsm_1 = FSM.new(1)
     @fsm_2 = FSM.new("Bruce_Banner")
 
-    @fsm_1.accept(1,2,3,4,5)
-    @fsm_2.accept("The_Incredible_Hulk")
+    @fsm_1.send(:accept, 1,2,3,4,5)
+    @fsm_2.send(:accept, "The_Incredible_Hulk")
 
-    @fsm_1.add_transition(2, 1, 3)
-    @fsm_1.add_transition(1, 1, 2)
-    @fsm_1.add_transition(3, 1, 1)
-    @fsm_2.add_transition("Bruce_Banner", "anger", "The_Incredible_Hulk")
+    @fsm_1.send(:add_transition, 2, 1, 3)
+    @fsm_1.send(:add_transition, 1, 1, 2)
+    @fsm_1.send(:add_transition, 3, 1, 1)
+    @fsm_2.send(:add_transition, "Bruce_Banner", "anger", "The_Incredible_Hulk")
   end
 
   def test_constructor
@@ -34,19 +34,19 @@ class TestFSM < Test::Unit::TestCase
   end
 
   def test_change_state
-    5.times {@fsm_1.change_state(1)}
+    5.times {@fsm_1.send(:change_state, 1)}
     assert_equal(3, @fsm_1.state)
-    assert_equal("The_Incredible_Hulk", @fsm_2.change_state("anger"))
-    assert_raise(NoMethodError) { @fsm_2.change_state("tired") }
+    assert_equal("The_Incredible_Hulk", @fsm_2.send(:change_state, "anger"))
+    assert_raise(NoMethodError) { @fsm_2.send(:change_state, "tired") }
   end
 
-  def test_get_array_of_words
-    test1 = FSM.get_array_of_words(" Hewl12o, , ,WoRlds, !")
+  def test_parse_input_line_for_words
+    test1 = FSM.parse_input_line_for_words(" Hewl12o, , ,WoRlds, !")
     assert_equal(["Hewl12o", "WoRlds"], test1)
   end
 
   def test_get_transition
-    words = FSM.get_array_of_words(" Hewl12o, , ,WoRlds, !")
+    words = FSM.parse_input_line_for_words(" Hewl12o, , ,WoRlds, !")
     chars = "test"
     i = 0
     FSM.get_transition(" Hewl12o, , ,WoRlds, !", true) do |transition|
@@ -61,18 +61,20 @@ class TestFSM < Test::Unit::TestCase
   end
 
   def test_build_from_file
-    fsm = FSM.build_from_file(File.dirname(__FILE__) + "/fsm_1")
+    fsm = FSM.new
+    fsm.build_from_file(File.dirname(__FILE__) + "/fsm_1")
     assert_equal(fsm.state, @fsm_1.state.to_s)
     5.times do 
-      @fsm_1.change_state(1)
-      fsm.change_state("1")
+      @fsm_1.send(:change_state, 1)
+      fsm.send(:change_state, "1")
     end
     assert_equal("3", fsm.state)
     assert_equal(fsm.state, @fsm_1.state.to_s)
   end
 
   def test_run
-    fsm_ends_in_0 = FSM.build_from_file(File.dirname(__FILE__) + "/fsm_ends_in_0")
+    fsm_ends_in_0 = FSM.new
+    fsm_ends_in_0.build_from_file(File.dirname(__FILE__) + "/fsm_ends_in_0")
     assert_equal("Accept", fsm_ends_in_0.run("1010101010"))
     assert_equal("Reject: invalid final state", fsm_ends_in_0.run("1010101011"))
     assert_equal("Reject: invalid transition", fsm_ends_in_0.run("1010101021"))
