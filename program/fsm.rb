d3aa6870809af8b40eac2470705533639a40ea97
Patrick <phloftus@gmail.com>
2015-04-05 17:21:47 -0400

    ndfsm

--- program/fsm.rb
+++ program/fsm.rb
@@ -53,26 +53,28 @@
 #
 class FSM
 
-  attr_reader :state, :states, :accepting_states, :paths
+  attr_accessor :state, :accepting_states, :paths, :eta, :curr_states, :next_states
 
   ## 
   #  Initializes empty fsm  
   #
   def initialize(state=nil)
     @state = state
-    @states = []
-    @states << state if state
+    @curr_states = []
+    @next_states = []
     @accepting_states = []
-    @transitions = []
-    @paths = {}
+    @paths = Hash.new{ |h1, k1| h1[k1] = 
+                       Hash.new{ |h2, k2| h2[k2] = [] }}
   end
 
   def clear
     @state = nil
-    @states = []
+    @eta = nil
+    @curr_states = []
+    @next_states = []
     @accepting_states = []
-    @transitions = []
-    @paths = {}
+    @paths = Hash.new{ |h1, k1| h1[k1] = 
+                       Hash.new{ |h2, k2| h2[k2] = [] }}
   end
 
   def accept(*args)
@@ -83,27 +85,7 @@ class FSM
   #  {Test Example}[rdoc-ref:TestFSM#test_add_path]
   #
   def add_path(start, path, dest)
-    if @paths.has_key?(start)
-      @paths[start] = @paths[start].merge({ path => dest })
-    else
-      @paths[start] = { path => dest }
-    end
-    if not @states.include? start
-      @states << start
-    end
-    if not @states.include? dest
-      @states << dest
-    end
-    if not @transitions.include? path
-      @transitions << path
-    end
-  end
-
-  ##
-  #  {Test Example}[rdoc-ref:TestFSM#test_change_state]
-  #
-  def change_state(input)
-    @state = @paths[@state][input]
+      @paths[start][path] << dest
   end
 
   ##
@@ -130,7 +112,7 @@ class FSM
       while (line = f.gets and line[/\#/]) # skip comments
       end
       @state = FSM.parse_input_line_for_words(line)[0]
-      @states << @state
+      @eta = FSM.parse_input_line_for_words(f.gets)[0]
       accept(FSM.parse_input_line_for_words(f.gets))
       while (line = f.gets) 
         start, path, dest = FSM.parse_input_line_for_words(line)
@@ -138,27 +120,25 @@ class FSM
       end
     end
   end
-      
+
   ##
-  #  Writes the fsm to a file using a formatting style
-  #  that is compatible with the build from file method
-  #  
-  def write_to_file(filename) 
-    open(filename, 'w+') do |f|
-      f.puts "#{@state}"
-      @accepting_states.each_with_index do |state, pos|
-        if pos <  @accepting_states.length-1
-          f.print "#{state}, "
-        else
-          f.print "#{state}\n"
+  #  Uses breadth first search to find all states that are reachable
+  #  from the start state
+  #
+  def get_reachable_states(state, trans)
+    reachable_states = []
+    djikstra_queue = Queue.new
+    djikstra_queue.enq state
+    while not djikstra_queue.empty?
+      visited_state = djikstra_queue.pop
+      if @paths[visited_state][trans]
+        @paths[visited_state][trans].each do |dest|
+          djikstra_queue.enq dest unless reachable_states.include? dest
         end
-      end
-      @paths.each do |src, paths_from_src|
-        paths_from_src.each do |trans, dest|
-          f.puts "#{src}, #{trans}, #{dest}"
-        end
+        reachable_states << visited_state unless visited_state.eql? state
       end
     end
+    return reachable_states
   end
 
   ##
@@ -185,19 +165,22 @@ class FSM
   # 
   def run(input)
     has_words = input.include?(",")
-    begin
-      FSM.get_transition(input, has_words) do |transition|
-        change_state(transition)
+    @curr_states = []
+    @next_states = []
+    @curr_states = get_reachable_states(@state, @eta)
+    FSM.get_transition(input, has_words) do |transition|
+      return "Reject" if @curr_states.empty?
+      @curr_states.each do |state|
+        @next_states.concat(get_reachable_states(state, transition))
+        @next_states.concat(get_reachable_states(state, @eta))
       end
-      if @accepting_states.include?(@state)
-        return "Accept"
-      else
-        return "Reject: invalid transition" if @state.nil?
-        return "Reject: invalid final state"
-      end
-    rescue NoMethodError
-      return "Reject: invalid transition"
+      @curr_states = @next_states
+      @next_states = []
     end
+    @curr_states.each do |state|
+      return "Accept" if @accepting_states.include?(state)
+    end
+    return "Reject"
   end
 
   ##
@@ -209,12 +192,10 @@ class FSM
   #
   def run_from_file(input_file, output_file)
     results = []
-    start_state = @state
     open(input_file, 'r') do |f|
       while (line = f.gets)
         line = line.strip unless line.nil?
         next if (line[0..1] == "//")
-        @state = start_state
         result = run(line)
         puts result unless output_file
         results << result
@@ -229,193 +210,14 @@ class FSM
     end
   end
 
-  ##
-  #  Uses breadth first search to find all states that are reachable 
-  #  from the start state
-  #
-  def get_reachable_states()
-    reachable_states = [@state]
-    djikstra_queue = Queue.new
-    djikstra_queue.enq @state
-    while not djikstra_queue.empty?
-      visited_state = djikstra_queue.pop
-      if @paths[visited_state]
-        @paths[visited_state].each do |trans, dest|
-          djikstra_queue.enq dest unless reachable_states.include? dest
-        end
-        reachable_states << visited_state
-      end
-    end
-    return reachable_states
-  end
-
-  ##
-  #  Gets reachable states and then eliminates all others
-  #
-  #  {Test Example}[rdoc-ref:TestFSM#test_eliminate_unreachable_states]
-  #
-  def eliminate_unreachable_states()
-    reachable_states = get_reachable_states
-    @states.delete_if{ |state| not reachable_states.include? state }
-    @accepting_states.delete_if{ |state| not reachable_states.include? state }
-    @paths.delete_if{ |state| not reachable_states.include? state }
-  end
-
-  ##
-  #  Returns a hash of hashes.
-  #
-  #  In the return hash, each key corresponds to
-  #  a state.  For each possible transition there
-  #  is a secondary hash where the key is the 
-  #  transition and the value is the state that
-  #  results from said transition.
-  #
-  #  {Test Example}[rdoc-ref:TestFSM#test_build_tables]
-  #
-  def build_tables(classes)
-    tbls = Hash.new
-    @states.each do |state|
-      tbl = build_table state, classes
-      tbls[state] = tbl
-    end
-    return tbls
-  end
-
-  ##
-  #  Returns a 2 dimensional hash.
-  #
-  #  Each state in the fsm is a key in the hash.
-  #  The keys hash to another hash that contains
-  #  metadata.  The metadata includes the key's class
-  #  as well as all of the possible transitions from
-  #  the key and the classes that the transitions lead to
-  #
-  def build_table(state, classes)
-    tbl = Hash.new
-    classes.each do |cur_class|
-      my_class = cur_class if cur_class.find { |targ| targ.eql? state }
-      tbl["my_class"] = my_class and break if my_class
-    end
-    @transitions.each do |transition| 
-      dest = @paths[state][transition] rescue nil
-      tbl[transition] = dest unless dest
-      if dest
-        classes.each do |cur_class|
-          dest_class = cur_class if cur_class.find { |targ| targ.eql? dest }
-          tbl[transition] = dest_class and break if dest_class
-        end
-      end
-    end
-    return tbl
-  end
-
-  ##
-  #  Group the tables by metadata.  This returns a hash where the keys are the
-  #  unique metadata entries, and the values are an array containing all of the
-  #  tbls that share the same metadata.
-  #
-  #  Add all states with identical metadata to the same equiv_class.
-  #  Return an array containing each equiv_class
-  #
-  def split_classes(tbls)
-    equiv_classes = []
-    tbls.group_by{ |state, metadata| metadata }.values.each do |tbls_with_equiv_metadata|
-      # group_by returns a hash where
-      equiv_class = []
-      tbls_with_equiv_metadata.each do |state, metadata|
-        equiv_class << state
-      end
-      equiv_classes << equiv_class
-    end
-    return equiv_classes
-  end
-
-  ##
-  #  Returns a 2 dimensional array.  Each subarray contains all states
-  #  that are part of the same equivalence class
-  #
-  #  {Test Example}[rdoc-ref:TestFSM#test_get_equiv_classes]
-  #
-  def get_equiv_classes(equiv_classes=nil)
-    old_equiv_classes = equiv_classes
-    if not old_equiv_classes
-      old_equiv_classes = @states.partition{ |state| @accepting_states.include? state }.reject(&:empty?)
-    end
-    tbls = build_tables old_equiv_classes
-    new_equiv_classes = split_classes(tbls)
-    new_equiv_classes = get_equiv_classes(new_equiv_classes) unless new_equiv_classes.eql?(old_equiv_classes) 
-    return new_equiv_classes
-  end
-
-  ##
-  #  Modifies the fsm so that it contains only the first state from each equiv class.
-  #  It also removes any classes that are unreachable
-  #
-  #  {Test Example}[rdoc-ref:TestFSM#test_minimize]
-  #
-  def minimize()
-    eliminate_unreachable_states
-    equiv_classes = get_equiv_classes
-    keeper_states = []
-    equiv_classes.each do |equiv_class| 
-      keeper_states << equiv_class.first
-    end
-    #reset state
-    @state = equiv_classes.find{ |equiv_class| equiv_class.include? @state}.first
-    # remove redundant states
-    @states = keeper_states
-    # remove redundant accepting states
-    @accepting_states.delete_if{ |state| not @states.include? state }
-    # reset paths 
-    @paths.each do |src, paths_from_src|
-      src_class = equiv_classes.find{ |equiv_class| equiv_class.include?(src) }
-      src_keeper_state = src_class.first
-      paths_from_src.each do |trans, dest|
-        dest_class = equiv_classes.find{ |equiv_class| equiv_class.include?(dest) }
-        dest_keeper_state = dest_class.first
-        # replace src and dest with keeper state from respective classes
-        if @paths.has_key?(src_keeper_state)
-          @paths[src_keeper_state] = @paths[src_keeper_state].merge({ trans => dest_keeper_state })
-        else
-          @paths[src_keeper_state] = { trans => dest_keeper_state }
-        end
-      end
-    end
-    # remove redundant paths
-    @paths.delete_if{ |state| not @states.include? state }
-  end
-
-  ##
-  #  Builds an fsm from the input file
-  #  Minimizes the fsm.
-  #  Writes the minimized fsm to a file of the same
-  #  name as the input file but with the addition .min
-  #  extention
-  #
-  def build_from_file_and_minimize(filename)
-    build_from_file(filename)
-    minimize()
-    write_to_file(filename+".min")
-  end
-
   if __FILE__ == $0
     fsm_file = ARGV[0]
     input_file = ARGV[1]
     output_file = ARGV[2]
-    min = ARGV[3]
-    if (min and not min.eql? "--min") or (ARGV[0..2].include? "--min")
-      puts "Sorry, error when processing command line input."
-      puts "Please see docs for proper usage."
-      exit 1
-    end
     fsm = FSM.new
-    fsm_min = FSM.new if min
     fsm.build_from_file(fsm_file)
-    fsm_min.build_from_file_and_minimize(fsm_file) if min
-    fsm_min.write_to_file fsm_file + ".min" if min
     fsm.run_from_file(input_file, output_file)
-    fsm_min.run_from_file(input_file, output_file+".min") if min
   end
 
-  private :accept, :add_path, :change_state
+  private :accept, :add_path
 end
